b53cbc5c8ff82405892635eeb7be21c6619d1806
ImBad <70207769+neofilisoft@users.noreply.github.com>
2026-02-14 18:25:53 +0700

    Add files via upload

---
+++ engine/lighting.py
@@ -0,0 +1,20 @@
+import pygame
+
+class DynamicLighting:
+    def __init__(self, res):
+        self.light_mask = pygame.Surface(res, pygame.SRCALPHA)
+        self.lights = []
+
+    def clear(self):
+        self.light_mask.fill((30, 30, 50))  # ambient
+
+    def add_light(self, pos, radius, color=(255, 200, 100)):
+        light_surf = pygame.Surface((radius * 2, radius * 2), pygame.SRCALPHA)
+        for r in range(radius, 0, -2):
+            alpha = int(150 * (1 - r/radius))
+            pygame.draw.circle(light_surf, (*color, alpha), (radius, radius), r)
+        self.light_mask.blit(light_surf, (pos[0]-radius, pos[1]-radius),
+                             special_flags=pygame.BLEND_RGBA_ADD)
+
+    def apply(self, target_surface):
+        target_surface.blit(self.light_mask, (0, 0), special_flags=pygame.BLEND_RGBA_MULT)
