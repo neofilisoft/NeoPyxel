68aecbd7da8558340faf829c45b2e6ffd40cf0df
ImBad <70207769+neofilisoft@users.noreply.github.com>
2026-02-14 18:26:53 +0700

    Add files via upload

---
+++ editor/editorscript_bridge.py
@@ -0,0 +1,13 @@
+import os
+
+class ScriptBridge:
+    def __init__(self):
+        self.plugins = []
+
+    def load_python_plugins(self, path="./plugins"):
+        if not os.path.exists(path):
+            os.makedirs(path)
+        print(f"NeoPyxel: Loading Python Plugins from {path}...")
+
+    def execute_js_logic(self, js_code):
+        print(f"Executing JavaScript: {js_code[:30]}")
