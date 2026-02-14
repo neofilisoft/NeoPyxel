333583adc0296cd6408eb6fd0d2e9bdc8fb76b96
ImBad <70207769+neofilisoft@users.noreply.github.com>
2026-02-14 19:17:07 +0700

    Update ui.py

--- editor/ui.py
+++ editor/ui.py
@@ -6,14 +6,12 @@ class EditorUI:
         self.font = pygame.font.SysFont("NotoSans", font_size)
         self.ui_color = (255, 255, 255)
 
-    def draw_status(self, surface, clock, info_dict):
+    def draw_status(self, backend, clock, info_dict):
         y_offset = 10
         info_dict["FPS"] = int(clock.get_fps())
         for key, value in info_dict.items():
-            text_surf = self.font.render(f"{key}: {value}", True, self.ui_color)
-            surface.blit(text_surf, (10, y_offset))
+            backend.draw_text(f"{key}: {value}", (10, y_offset), self.ui_color, self.font)
             y_offset += 25
 
-    def draw_cursor_info(self, surface, pos):
-        coord_text = self.font.render(f"Pos: {pos}", True, (200, 200, 0))
-        surface.blit(coord_text, (pos[0] + 10, pos[1] + 10))
+    def draw_cursor_info(self, backend, pos):
+        backend.draw_text(f"Pos: {pos}", (pos[0] + 10, pos[1] + 10), (200, 200, 0), self.font)
